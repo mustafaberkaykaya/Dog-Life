758c0b70da8268f1a3fb9ca36298302577a08c4b
Berkay  Kaya <berkaykaya_2000@hotmail.com>
2020-08-24 15:17:16 +0300

    Initial Commit

---
+++ DogLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogLife
+//
+//  Created by Mustafa Berkay Kaya on 24.08.2020.
+//  Copyright © 2020 Berkay Kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
